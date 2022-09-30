13f1df7ed25b7b2f406723f3a5ca7739eb0f033f
aman9893 <aman9893jain@gmail.com>
2022-09-30 11:43:51 +0530

    add

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: 'Aman12345@',
-    database: "restro",
-    multipleStatements: true
-    // reconnect:true,
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'restro',
+    port: 3306,
+    debug: true,
+    connectionLimit: 5
 });
 connection.connect(function(err) {
     if (!err) {
