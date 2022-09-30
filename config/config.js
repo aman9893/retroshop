fa2375d64e9674c6aa1eeb832b60fb0015f03d57
aman9893 <aman9893jain@gmail.com>
2022-09-30 15:43:34 +0530

    fsdfd

--- config/config.js
+++ config/config.js
@@ -4,15 +4,15 @@ var connection = mysql.createConnection({
     user: 'root',
     password: 'password',
     database: 'restro',
-    port: 3306,
-    debug: true,
-    connectionLimit: 5
+    port: '3306',
+    connectTimeout: 60000,
 });
 connection.connect(function(err) {
     if (!err) {
         console.log("Database is connected");
     } else {
         console.log(err);
+        connection.end();
     }
 });
 
