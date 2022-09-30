e5019e8d8115b01701090903c82ad9f0975eb11a
aman9893 <aman9893jain@gmail.com>
2022-09-30 16:18:04 +0530

    gchg

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'ec2-43-206-128-164.ap-northeast-1.compute.amazonaws.com',
+    host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'restro',
