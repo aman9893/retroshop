7b0e683d83b49ea413ccc17efd96ef9bf39b5d3e
aman9893 <aman9893jain@gmail.com>
2024-01-11 00:54:53 +0530

    aded

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: '18.217.110.106',
+    host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'restro',
