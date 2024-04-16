35c07cfb9a13429a7aac944dd7e262bf7ff0ce4e
aman9893 <aman9893jain@gmail.com>
2024-04-16 18:29:30 +0530

    add

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use((req, res, next) => {
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
   next();
 });
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
   app.use(cors());
   app.all('*', function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
