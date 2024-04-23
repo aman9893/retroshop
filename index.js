80e7c914754cd9542e4e50ba00506bf8bbc645c7
aman9893 <aman9893jain@gmail.com>
2024-04-23 18:04:53 +0530

    add

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 
-const express = require('express')
-const routes = require('./router/router')
-const path = require('path')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+// const express = require('express')
+// const routes = require('./router/router')
+// const path = require('path')
+// const cors = require('cors')
+// const bodyParser = require('body-parser')
+// const app = express()
+// const port = '3011'
+// app.set('views', path.join(__dirname,'views'));
+// app.set('view engine','html');
+
+// app.use(express.urlencoded({extended: false}))
+// app.use(express.json())
+
+// app.use((req, res, next) => {
+//   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+//   next();
+// });
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+//   app.use(cors());
+//   app.all('*', function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//    next();
+// });
+// app.use('/', routes)
+
+// app.get('/test', (req,res) => res.send('Hello World'))
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, () => {
+//     console.log("Server is running....")
+// })
+
+
+const express = require("express")
 const app = express()
-const port = '3011'
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','html');
+
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use((req, res, next) => {
-  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-  next();
-});
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-  app.use(cors());
-  app.all('*', function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-   next();
-});
-app.use('/', routes)
 
 app.get('/test', (req,res) => res.send('Hello World'))
 
